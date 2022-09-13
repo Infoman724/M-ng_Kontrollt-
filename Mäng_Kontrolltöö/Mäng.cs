a51f33a72233b9ef28ff04039453b80698b4d5e4
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-09-13 15:28:51 +0300

    4,4.1 tehtud

---
+++ Mäng_Kontrolltöö/Mäng.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mäng_Kontrolltöö
+{
+    internal class Mäng
+    {
+        Tegelane[] tegelane;//4.0 tehtud
+        public Mäng(Tegelane[] tegelane)
+        {
+            this.tegelane = tegelane;//4.1 tehtud
+        }
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+}
