a3100ead46da7a245608799dc1ee4d35cc1f0f4e
Infoman724 <93324312+Infoman724@users.noreply.github.com>
2022-09-13 11:00:42 +0300

    Добавьте файлы проекта.

---
+++ Mäng_Kontrolltöö/Ese.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mäng_Kontrolltöö
+{
+    public class Ese : IÜksus
+    {
+        public string info()
+        {
+            throw new NotImplementedException();
+        }
+        //2.1 tehtud
+        public int punktideArv()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ese(string info,int punktideArv);
+
+    
+    
+    
+    }
+}
---
+++ Mäng_Kontrolltöö/IÜksus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mäng_Kontrolltöö
+{
+    public interface IÜksus
+    {
+        int punktideArv();
+        //1-ülesanne tehtud
+        string info();
+    }
+}
